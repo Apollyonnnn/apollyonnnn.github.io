f8d27c4b31cb153ffaa1f1c08198b9f4fe5b2d6b
Apollyonnnn <128554880+Apollyonnnn@users.noreply.github.com>
2023-03-21 20:26:19 -0400

    1

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ var kills = 0;
 
 function enemyClick(number){
     kills = kills + number;
-    document.getElementById("Kills").innerHTML = kills;
+    document.getElementById("kills").innerHTML = kills;
 };
