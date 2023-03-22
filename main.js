9d0ac43d5370799dd3711b0e07354611cd4133ea
Apollyonnnn <128554880+Apollyonnnn@users.noreply.github.com>
2023-03-21 21:39:33 -0400

    stuff

--- main.js
+++ main.js
@@ -1,42 +1,39 @@
-var strength = 0;
+var gold = 0;
 
-function treepunchClick(number){
-    strength = strength + number;
-    document.getElementById("strength").innerHTML = strength;
+function click(number){
+    gold = gold + number;
+    document.getElementById("gold").innerHTML = gold;
 };
 
-var units = 0;
-var axes = 0;
+var fames = 0;
+var efficiencys = 0;
 
-function buyUnit(){
-    var unitCost = Math.floor(10 * Math.pow(1.1,units));     //works out the cost of this unit
-    if(strength >= unitCost){                                   //checks that the player can afford the unit
-        units = units + 1;                                   //increases number of units
-    	strength = strength - unitCost;                          //removes the units spent
-        document.getElementById('units').innerHTML = units;  //updates the number of units for the user
-        document.getElementById('strength').innerHTML = strength;  //updates the number of units for the user
+function buyFame(){
+    var fameCost = Math.floor(10 * Math.pow(1.1,fames));     //works out the cost of this fame
+    if(gold >= fameCost){                                   //checks that the player can afford the fame
+        fames = fames + 1;                                   //increases number of fames
+    	gold = gold - fameCost;                          //removes the fames spent
+        document.getElementById('fames').innerHTML = fames;  //updates the number of fames for the user
+        document.getElementById('gold').innerHTML = gold;  //updates the number of fames for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,units));       //works out the cost of the next unit
-    document.getElementById('unitCost').innerHTML = nextCost;  //updates the unit cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,fames));       //works out the cost of the next fame
+    document.getElementById('fameCost').innerHTML = nextCost;  //updates the fame cost for the user
 };
 
-function buyAxe(){
-    var axeCost = Math.floor(10 * Math.pow(1.1,axes));     //works out the cost of this axe
-    if(strength >= axeCost){                                   //checks that the player can afford the axe
-        axes = axes + 1;                                   //increases number of axe
-    	strength = strength - axeCost;                          //removes the axe spent
-        document.getElementById('axes').innerHTML = axes;  //updates the number of axe for the user
-        document.getElementById('strength').innerHTML = strength;  //updates the number of axe for the user
+function buyEfficiency(){
+    var efficiencyCost = Math.floor(10 * Math.pow(1.1,efficiencys));     //works out the cost of this efficiency
+    if(gold >= efficiencyCost){                                   //checks that the player can afford the efficiency
+        efficiencys = efficiencys + 1;                                   //increases number of efficiency
+    	gold = gold - efficiencyCost;                          //removes the efficiency spent
+        document.getElementById('efficiencys').innerHTML = efficiencys;  //updates the number of efficiency for the user
+        document.getElementById('gold').innerHTML = gold;  //updates the number of efficiency for the user
     };
-    var nextCost = Math.floor(10 * Math.pow(1.1,axes));       //works out the cost of the next axe
-    document.getElementById('axeCost').innerHTML = nextCost;  //updates the axe cost for the user
+    var nextCost = Math.floor(10 * Math.pow(1.1,efficiencys));       //works out the cost of the next efficiency
+    document.getElementById('efficiencyCost').innerHTML = nextCost;  //updates the efficiency cost for the user
 };
 
 window.setInterval(function(){
 	
-	treepunchClick(units/100);
-    units = units * 10000;
-    Math.round(2, units);
-    units = units / 10000;
+	click(fames);
 	
-}, 10);
+}, 1000);
