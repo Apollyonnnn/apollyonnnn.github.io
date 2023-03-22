7bf82bf1025d65151471d159a46e57f59b5b1871
Apollyonnnn <128554880+Apollyonnnn@users.noreply.github.com>
2023-03-21 21:08:33 -0400

    fixed the axes/units bug + some number stuff

--- main.js
+++ main.js
@@ -25,15 +25,18 @@ function buyAxe(){
     if(strength >= axeCost){                                   //checks that the player can afford the axe
         axes = axes + 1;                                   //increases number of axe
     	strength = strength - axeCost;                          //removes the axe spent
-        document.getElementById('units').innerHTML = axes;  //updates the number of axe for the user
+        document.getElementById('axes').innerHTML = axes;  //updates the number of axe for the user
         document.getElementById('strength').innerHTML = strength;  //updates the number of axe for the user
     };
     var nextCost = Math.floor(10 * Math.pow(1.1,axes));       //works out the cost of the next axe
-    document.getElementById('unitCost').innerHTML = nextCost;  //updates the axe cost for the user
+    document.getElementById('axeCost').innerHTML = nextCost;  //updates the axe cost for the user
 };
 
 window.setInterval(function(){
 	
 	treepunchClick(units/100);
+    units = units * 10000;
+    Math.round(2, units);
+    units = units / 10000;
 	
 }, 10);
